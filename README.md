288a48e0643f4075051f1a224f8e99b4dbdfbdc5
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-14 15:56:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# random_phrases
+ show random phrases - Course Udemy, Jamilton Damasceno
---
+++ android/app/src/main/java/com/andrericardoweb/random_phrases/MainActivity.java
@@ -0,0 +1,6 @@
+package com.andrericardoweb.random_phrases;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
