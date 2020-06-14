c8ccb8ccac466835b33a9a9cc4f31bbbf1f97e68
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-14 16:43:50 -0300

    showing random phrase

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +7,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var _phrases = [
+    "Os problemas são oportunidades para se mostrar o que sabe. (Duke Ellington)",
+    "Nossos fracassos, às vezes, são mais frutíferos do que os êxitos. (Henry Ford)"
+    "Tente de novo. Fracasse de novo. Mas fracasse melhor. (Samuel Beckett)"
+    "É costume de um tolo, quando erra, queixar-se dos outros. É costume de um sábio queixar-se de si mesmo. (Sócrates)"
+    "O verdadeiro heroísmo consiste em persistir por mais um momento, quando tudo parece perdido. (W. F. Grenfel)"
+    "Cada cliente é como se fosse primeiro e único. (Norman Bawes)"
+    "Mesmo que já tenhas feito uma longa caminhada, há sempre um novo caminho a fazer. (Santo Agostinho)"
+  ];
+
+  var _generatedPhrase = "Clique abaixo para gerar uma frase";
+
+  void _generatePhrase() {
+    var randomNumber = Random().nextInt(_phrases.length);
+    setState(() {
+      _generatedPhrase = _phrases[randomNumber];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +44,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Image.asset("images/logo.png"),
                 Text(
-                  "Clique abaixo para gerar uma frase!",
+                  _generatedPhrase,
                   textAlign: TextAlign.justify,
                   style: TextStyle(
                       fontSize: 17,
@@ -39,7 +60,7 @@ class _HomeState extends State<Home> {
                         fontWeight: FontWeight.bold),
                   ),
                   color: Colors.green,
-                  onPressed: () {},
+                  onPressed: _generatePhrase,
                 )
               ],
             ),
